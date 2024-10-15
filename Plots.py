6ed0b13de60a0417662b3c7d2b3f101a6bf79327
yue <esff2299@gmail.com>
2024-10-15 10:21:57 +0800

    smiles may not exisit in database

--- Plots.py
+++ Plots.py
@@ -5,7 +5,8 @@ def plot_sigma_profile(sigma_profile, prediction, smiles, id=None):
     density = np.round(np.arange(-0.025, 0.0251, 0.001), 4)
 
     plt.figure(figsize=(8, 6))
-    plt.plot(density, sigma_profile, marker='o', linestyle='-', color='dodgerblue', markersize=5, label='Real Profile')
+    if sigma_profile:
+        plt.plot(density, sigma_profile, marker='o', linestyle='-', color='dodgerblue', markersize=5, label='Real Profile')
     plt.plot(density, prediction, marker='s', linestyle='--', color='r', markersize=5, label='Prediction')
 
     plt.xlabel(r'$\sigma(e/\AA^2)$')
