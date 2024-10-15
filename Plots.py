084c032e6b91a1e621609a9a6020fd369531e613
yue <esff2299@gmail.com>
2024-10-15 09:57:52 +0800

    add Plots

---
+++ Plots.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_sigma_profile(sigma_profile, prediction, smiles, id=None):
+    density = np.round(np.arange(-0.025, 0.0251, 0.001), 4)
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(density, sigma_profile, marker='o', linestyle='-', color='dodgerblue', markersize=5, label='Real Profile')
+    plt.plot(density, prediction, marker='s', linestyle='--', color='r', markersize=5, label='Prediction')
+
+    plt.xlabel(r'$\sigma(e/\AA^2)$')
+    plt.ylabel(r'P($\sigma$)')
+    if id:
+        plt.title(f'{id}, {smiles}')
+    else:
+        plt.title(smiles)
+    plt.grid(True)
+    plt.legend()
+
+    plt.show()
