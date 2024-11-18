38bc2b5d4b59ac97f4c9dd307de8ec5623a4584f
yue <esff2299@gmail.com>
2024-11-18 21:16:54 +0800

    improvement

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ class COSBERT(nn.Module):
         
         # dimension of hidden layers
         hidden_dim_1 = 512
-        hidden_dim_last = 256
+        hidden_dim_2 = 256
         
         # Component Embedding Network f_theta Input: E_i Output: V_COSMO, A_COSMO, sigma profile
         
@@ -19,13 +19,13 @@ class COSBERT(nn.Module):
             nn.Linear(Embedding_ChemBERT, hidden_dim_1),
             nn.Dropout(p=0.5),
             nn.ReLU(),
-            nn.Linear(hidden_dim_1, hidden_dim_last),
+            nn.Linear(hidden_dim_1, hidden_dim_2),
             nn.Dropout(p=0.4),
             nn.ReLU()
         )
         # V_cosmo and A_cosmo output layer [2]
         self.V_A_output = nn.Sequential(
-            nn.Linear(hidden_dim_last, 16),
+            nn.Linear(hidden_dim_2, 16),
             nn.ReLU(),
             nn.Linear(16, 2),
             nn.LeakyReLU(negative_slope=0.01) # avoid the vanishing gradient (output is 0) problem in some situation, reduce "dead neurons"
@@ -35,9 +35,12 @@ class COSBERT(nn.Module):
         
         # sigma profile output layer [51]
         self.sigma_output = nn.Sequential(
-            nn.Linear(hidden_dim_last, 64),
+            nn.Linear(hidden_dim_2, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Linear(64, 51)
+            nn.Linear(128, 51),
+            nn.ReLU(),
+            nn.Linear(64, 51),
             # nn.Sigmoid() # restrict the outputs between 0 and 1
         )
 
