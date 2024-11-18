6974c45155f8d0abd429013e75e25819567f01e5
yue <esff2299@gmail.com>
2024-11-18 15:49:26 +0800

    add normalization of V and A

--- model.py
+++ model.py
@@ -3,9 +3,10 @@ import torch
 import torch.nn.functional as F
 
 class COSBERT(nn.Module):
-    def __init__(self, Embedding_ChemBERT=384):
+    def __init__(self, mean_VA, std_VA, Embedding_ChemBERT=384):
         super(COSBERT, self).__init__()
-
+        self.mean_VA = mean_VA
+        self.std_VA = std_VA
         # self.Embedding_ChemBERT = Embedding_ChemBERT # Pre-trained embeddings (E_i) from ChemBERTa-2
         
         # dimension of hidden layers
@@ -27,7 +28,9 @@ class COSBERT(nn.Module):
             nn.Linear(hidden_dim_last, 16),
             nn.ReLU(),
             nn.Linear(16, 2),
-            nn.ReLU()
+            nn.LeakyReLU(negative_slope=0.01) # avoid the vanishing gradient (output is 0) problem in some situation, reduce "dead neurons"
+            # x  if x > 0
+            # ax if x <= 0
         )
         
         # sigma profile output layer [51]
@@ -41,13 +44,14 @@ class COSBERT(nn.Module):
     def forward(self, E_i):
         shared_out = self.shared_layers(E_i)
         
+        # Normalized output of V_cosmo and A_cosmo
         V_A_out = self.V_A_output(shared_out)
+        V_A_out = (V_A_out - self.mean_VA) / self.std_VA
         
+        # sigma profile
         sigma_out = self.sigma_output(shared_out)
         sigma_out = torch.relu(sigma_out) # prevent negative values
-        
         sigma_sum = torch.sum(sigma_out, dim=1, keepdim=True) + 1e-8 # prevent sum = 0
-        
         sigma_out = sigma_out / sigma_sum # normalization
         
         return torch.cat((V_A_out, sigma_out), dim=1) # [B,53]
